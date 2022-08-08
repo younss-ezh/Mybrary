bab18761e6a9085165c4c5f569a03bf9270d7b57
younss-ezh <younssezh63@gmail.com>
2022-08-08 19:34:14 +0200

    File Upload database

--- models/book.js
+++ models/book.js
@@ -1,46 +1,45 @@
-const mongoose = require('mongoose') ;
-const path = require('path')
+const mongoose = require('mongoose')
 
-const coverImageBasePath = 'uploads/bookCovers'
-
-const bookShema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    discription: {
-        type: String
-    },
-    publishDate: {
-        type: Date,
-        required: true
-    },
-    pageCount: {
-        type: Number,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    },
-    coverImageName: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Author'
-    }
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  publishDate: {
+    type: Date,
+    required: true
+  },
+  pageCount: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  coverImage: {
+    type: Buffer,
+    required: true
+  },
+  coverImageType: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Author'
+  }
 })
 
-bookShema.virtual('coverImagePath').get(function() {
-    if ( this.coverImageName != null ) {
-        return path.join('/', coverImageBasePath, this.coverImageName)
-    }
+bookSchema.virtual('coverImagePath').get(function() {
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+  }
 })
 
-
-module.exports = mongoose.model('Book', bookShema) ;
-module.exports.coverImageBasePath = coverImageBasePath
+module.exports = mongoose.model('Book', bookSchema)
