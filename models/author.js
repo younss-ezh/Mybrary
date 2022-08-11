1435daffe653b97e237908464858ae62a11e7d44
younss-ezh <younssezh63@gmail.com>
2022-08-11 02:22:01 +0200

    Web Style Finish

--- models/author.js
+++ models/author.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose') ;
+const Book = require('./book')
 
 const authorShema = new mongoose.Schema({
     name: {
-        type: String,
+        type: String,   
         required: true
     }
+})
+
+authorShema.pre('remove', function(next) {
+    Book.find({author: this.id}, (err, books) => {
+        if(err) {
+            next(err)
+        } else if(books.length > 0) {
+            next(new Error("This Author Stille Have Books"))
+        } else {
+            next()
+        }
+    })
 })
 
 module.exports = mongoose.model('Author', authorShema) ;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
   const app = express() ;
   const expressLayouts = require('express-ejs-layouts') ;
   const bodyparser = require('body-parser') ;
+  const methodOverride = require('method-override') ;
   
   const indexRouter = require('./routes/index') ;
   const authorRouter = require('./routes/authors') ;
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.set('views', __dirname + '/views') ;
   app.set('layout', 'layouts/layout') ;
   app.use(expressLayouts)
+  app.use(methodOverride('_method'))
   app.use(express.static('public')) ;
   app.use(bodyparser.urlencoded({ limit: '10mb', extended: false }))
   
